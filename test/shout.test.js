b1559bb9ae8326d7876a115c01c3029516e4dc72
juampi92 <juampi92@gmail.com>
2016-02-28 20:27:08 -0300

    Fav and unfav new way. Delete shout

--- test/shout.test.js
+++ test/shout.test.js
@@ -5,7 +5,7 @@ module.exports = function(instance) {
   describe('Shouts', function() {
 
     it('should download and check ', function(done) {
-      this.timeout(15000);
+      this.timeout(5000);
 
       instance.shout.get(68970580).then(function(success, shout) {
         shout.owner.id.should.be.equal(21271542);
@@ -17,7 +17,7 @@ module.exports = function(instance) {
     });
 
     it('should like a shout ', function(done) {
-      this.timeout(15000);
+      this.timeout(5000);
 
       instance.shout.like(68982830).then(function(success, message, response) {
         if (success) {
@@ -33,7 +33,41 @@ module.exports = function(instance) {
     });
 
     it('should unlike a shout ', function(done) {
+      this.timeout(5000);
+
       instance.shout.unlike(68982830).then(function(success) {
+        if (success) {
+          done();
+        } else {
+          // The user MUST have liked the shout in the previous test
+          throw new Error('There was an error disliking that shout');
+        }
+      }, function(err) {
+        throw new Error(err);
+      });
+
+    });
+
+    it('should fav a shout ', function(done) {
+      this.timeout(5000);
+
+      instance.shout.fav(68982830).then(function(success, message, response) {
+        if (success) {
+          done();
+        } else {
+          // The user may have liked the shout already
+          console.error('You have already liked that shout? Response: ', message);
+          done();
+        }
+      }, function(err) {
+        throw new Error(err);
+      });
+    });
+
+    it('should unfav a shout ', function(done) {
+      this.timeout(5000);
+
+      instance.shout.unfav(68982830).then(function(success) {
         if (success) {
           done();
         } else {
